7ec822109fc71c2e58589c02fdb7bca88aa12dfc
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-12-03 11:06:23 +0100

    Rename variable

--- 01/01.js
+++ 01/01.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 fs.readFile('./01.data', 'utf-8', (err, data) => {
   const leftParens = data.match(/\(/g).length;
   const rightParens = data.match(/\)/g).length;
-  const total = leftParens - rightParens;
+  const result = leftParens - rightParens;
 
-  console.log(total);
+  console.log(result);
 });
