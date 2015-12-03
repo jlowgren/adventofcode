eb5a45e7a9e5b824e7de381ebd8ece3b2be661e4
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-12-03 11:02:21 +0100

    Clean up

--- 01/01.js
+++ 01/01.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 fs.readFile('./01.data', 'utf-8', (err, data) => {
   const leftParens = data.match(/\(/g).length;
   const rightParens = data.match(/\)/g).length;
+  const total = leftParens - rightParens;
 
-  console.log(`(: ${leftParens}\n): ${rightParens}\nResult: ${leftParens - rightParens}`);
+  console.log(total);
 });
